3c8f9ac777a2cb5bae65041a6449e470118b96fb
uriel-hapuq <uhbsantos.cic@uesc.br>
2025-01-27 02:31:27 -0300

    refatorando
    trocando if...else por switch-case e movendo arrays de desenhos para diretorios a parte

---
+++ desenhos/desenhos.h
@@ -0,0 +1,31 @@
+#ifndef DESENHOS_H
+#define DESENHOS_H
+
+#define NUM_PIXELS 25
+
+extern double desenho1[NUM_PIXELS];
+extern double desenho2[NUM_PIXELS];
+extern double letraT[NUM_PIXELS];
+extern double letraE[NUM_PIXELS];
+extern double letraC[NUM_PIXELS];
+extern double letraH[NUM_PIXELS];
+extern double exclamacao[NUM_PIXELS];
+extern double numero1[NUM_PIXELS];
+extern double numero2[NUM_PIXELS];
+extern double numero3[NUM_PIXELS];
+extern double numero4[NUM_PIXELS];
+extern double numero5[NUM_PIXELS];
+extern double ledsLigados[NUM_PIXELS];
+extern double desenho_white[NUM_PIXELS];
+extern double desenhocorner1[NUM_PIXELS];
+extern double desenhocorner2[NUM_PIXELS];
+extern double desenhocorner3[NUM_PIXELS];
+extern double desenhocorner4[NUM_PIXELS];
+extern double desenhocorner5[NUM_PIXELS];
+extern double desenhomiddle1[NUM_PIXELS];
+extern double desenhomiddle2[NUM_PIXELS];
+extern double desenhomiddle3[NUM_PIXELS];
+extern double desenhomiddle4[NUM_PIXELS];
+extern double desenhomiddle5[NUM_PIXELS];
+
+#endif
