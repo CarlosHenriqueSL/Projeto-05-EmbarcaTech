e1cd64dc948f05abf6ac3f3e0752cdef247e805c
Carlos Henrique <luiscarloslopes597@gmail.com>
2025-02-01 15:17:35 -0300

    Primeiro commit, enviando criação do código para o repositório.

--- desenhos/desenhos.h
+++ desenhos/desenhos.h
@@ -3,62 +3,16 @@
 
 #define NUM_PIXELS 25
 
-extern double desenho1[NUM_PIXELS];
-extern double desenho2[NUM_PIXELS];
-extern double letraT[NUM_PIXELS];
-extern double letraE[NUM_PIXELS];
-extern double letraC[NUM_PIXELS];
-extern double letraH[NUM_PIXELS];
-extern double exclamacao[NUM_PIXELS];
 extern double numero1[NUM_PIXELS];
 extern double numero2[NUM_PIXELS];
 extern double numero3[NUM_PIXELS];
 extern double numero4[NUM_PIXELS];
 extern double numero5[NUM_PIXELS];
-extern double ledsLigados[NUM_PIXELS];
-extern double ledsDesligados[NUM_PIXELS];
-extern double desenho_white[NUM_PIXELS];
-extern double desenhocorner1[NUM_PIXELS];
-extern double desenhocorner2[NUM_PIXELS];
-extern double desenhocorner3[NUM_PIXELS];
-extern double desenhocorner4[NUM_PIXELS];
-extern double desenhocorner5[NUM_PIXELS];
-extern double desenhomiddle1[NUM_PIXELS];
-extern double desenhomiddle2[NUM_PIXELS];
-extern double desenhomiddle3[NUM_PIXELS];
-extern double desenhomiddle4[NUM_PIXELS];
-extern double desenhomiddle5[NUM_PIXELS];
-extern double circulo[NUM_PIXELS];
-extern double coracao[NUM_PIXELS];
-extern double setaBaixo[NUM_PIXELS];
-extern double setaCima[NUM_PIXELS];
-extern double quadrado[NUM_PIXELS];
-extern double estrela[NUM_PIXELS];
-extern double coroa[NUM_PIXELS];
-extern double setaEsquerda[NUM_PIXELS];
-extern double setaDireita[NUM_PIXELS];
-extern double rosto[NUM_PIXELS];
-extern void setIntensidade(double desenho[], double intensidade);
-extern double animacao_5_frame1[NUM_PIXELS];
-extern double animacao_5_frame2[NUM_PIXELS];
-extern double animacao_5_frame3[NUM_PIXELS];
-extern double animacao_5_frame4[NUM_PIXELS];
-extern double animacao_5_frame5[NUM_PIXELS];
-extern double animacao_6_frame1[NUM_PIXELS];
-extern double animacao_6_frame2[NUM_PIXELS];
-extern double animacao_6_frame3[NUM_PIXELS];
-extern double animacao_6_frame4[NUM_PIXELS];
-extern double animacao_6_frame5[NUM_PIXELS];
-extern double desenho_numero7a[NUM_PIXELS];
-extern double desenho_numero7b[NUM_PIXELS];
-extern double desenho_numero7c[NUM_PIXELS];
-extern double desenho_numero7d[NUM_PIXELS];
-extern double desenho_numero8a[NUM_PIXELS];
-extern double desenho_numero8b[NUM_PIXELS];
-extern double desenho_numero8c[NUM_PIXELS];
-extern double desenho_numero8d[NUM_PIXELS];
-extern double desenho_numero8e[NUM_PIXELS];
-extern double desenho_numero8f[NUM_PIXELS];
+extern double numero6[NUM_PIXELS];
+extern double numero7[NUM_PIXELS];
+extern double numero8[NUM_PIXELS];
+extern double numero9[NUM_PIXELS];
+extern double numero0[NUM_PIXELS];
 
 
 #endif
